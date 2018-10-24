3e536263634a68ea2cd2a609603600644a71a9bc
Kiril Peyanski <peyanski@progress.com>
2018-10-24 08:41:55 +0300

    Updates

---
+++ precache-manifest.69021f2dfa6a05915c12dc7c7dc2452d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31c294a27dbfd9697430",
+    "url": "/kendo-react-material-dashboard/static/js/runtime~main.31c294a2.js"
+  },
+  {
+    "revision": "e2689112ec0c2ea43c8d",
+    "url": "/kendo-react-material-dashboard/static/js/main.e2689112.chunk.js"
+  },
+  {
+    "revision": "10f28309fd3cfe0f34b7",
+    "url": "/kendo-react-material-dashboard/static/js/1.10f28309.chunk.js"
+  },
+  {
+    "revision": "e2689112ec0c2ea43c8d",
+    "url": "/kendo-react-material-dashboard/static/css/main.068c6d18.chunk.css"
+  },
+  {
+    "revision": "c96a37bfa581dc4761a6c92980890b85",
+    "url": "/kendo-react-material-dashboard/index.html"
+  }
+];
