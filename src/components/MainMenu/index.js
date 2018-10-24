ee2dc08182986dc75349d16600cb0a0a8339a972
Kiril Peyanski <peyanski@progress.com>
2018-10-24 09:29:12 +0300

    chore: fix redirects routing

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -58,7 +58,7 @@ class MainMenu extends Component {
                             </div>
                         </div>)}
                         <div className={'content-wrapper col'}>
-                            <Redirect from="/" to="dashboard" />
+                            <Redirect from="*" to="/dashboard" />
                             <Route path="/dashboard" component={Dashboard} />
                             <Route path="/issues" component={IssuesIndex} />
                             <Route path="/profile" component={withGithubApi(withGithubUser(Profile))} />
